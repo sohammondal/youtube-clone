7682987845895a9417f064464c329a37e5e0bf1b
sohammondal <soham.pf.mondal@gmail.com>
2020-05-01 21:57:51 +0530

    add SerachBar component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <input @input="onInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  methods: {
+    onInput: function(e) {
+      console.log(e.target.value);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
